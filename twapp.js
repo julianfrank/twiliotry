eda01f4d2ce05c69c22306bab4be43ff4cafb2db
Julian Frank <Jfempire@yahoo.Co.uk>
2017-07-17 09:59:37 +0000

    not working!

--- twapp.js
+++ twapp.js
@@ -1,29 +1,80 @@
-const serverPort=process.env.PORT||8080,
-serverIP=process.env.IP||'0.0.0.0'
-const xml = require('xml'),
-responseData={Response:[
-    {
-        Gather:[{
-        _attr:{
-            input:"speech",
-            action:"/result",
-            partialResultCallback:"/partialresult"
-        }        },
-        {Say:"Hi Buddy"}        ]
+const serverPort = process.env.PORT || 8080,
+    serverIP = process.env.IP || '0.0.0.0'
+const querystring = require('querystring'),
+    xml = require('xml'),
+    callJSON = {
+        Response: [{
+            Gather: [{
+                _attr: {
+                    input: "speech",
+                    action: "/result",
+                    partialResultCallback: "/partialresult"
+                }
+            }, {
+                Say: "Hi Buddy"
+            }]
+        }]
+    },
+    resultJSON = {
+        Say: "Hi Buddy Result"
+    },
+    pResultJSON = {
+        Say: "Hi Buddy Pee Pee Pee result"
     }
-    ]}
 
-const $ = xml(responseData, { declaration: true })
-console.log(responseData,$)
+const callXML = xml(callJSON, {
+        declaration: true
+    }),
+    resultXML = xml(resultJSON, {
+        declaration: true
+    }),
+    pResultXML = xml(pResultJSON, {
+        declaration: true
+    })
 
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end();
-});
+    var body = "",
+        bodyJson = {}
+    req.on('data', (chunk) => {
+        body += chunk
+    });
+    req.on('end', () => {
+        bodyJson = querystring.parse(body)
+        console.log('Body', bodyJson)
+
+        switch (req.url) {
+            case "/":
+                console.log(req.method, req.url)
+                res.write(req.method + req.url + JSON.stringify(req.headers))
+                break
+            case "/call":
+                console.log(req.method, req.url, "Sending:", callXML)
+                res.statusCode=200
+                res.write(callXML)
+                break
+            case "/result":
+                console.log(req.method, req.url, "Sending:", resultXML)
+                res.statusCode=200
+                res.write(resultXML)
+                break
+            case "/partialresult":
+                console.log(req.method, req.url, "Sending:", pResultXML)
+                res.statusCode=200
+                res.write(pResultXML)
+                break
+            default:
+            res.statusCode=404
+                res.write(req.method + req.url)
+                console.log(req.method, req.url, req.headers["referer"])
+        }
+        res.end()
+    })
+})
 server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
-server.listen(serverPort,serverIP,511,function(){
-    console.log("Listing on ",server.address())
+server.listen(serverPort, serverIP, 511, function() {
+    console.log("Listening on ", server.address())
 })
